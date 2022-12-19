8fa91f99e68a62ee83df52e9dff2f930c5bd70c3
danielmenezesbr <danielmenezes@gmail.com>
2022-12-19 18:00:15 +0000

    changed "Hello World" to be more like "Hello World" from Rocket official website

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/hello", routes![index])
+    rocket::build().mount("/", routes![hello])
 }
